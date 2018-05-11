b99c130ace8454acb9b3d20d320799ecf33f781b
Nicholas Klingler <ngklingler@yoga.lan>
2018-05-11 17:45:13 -0400

    fix CARlit in main to CAR

--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
 #include "bomb.h"
 #include "wire.h"
 #include "button.h"
+#include "memory.h"
 
 //extern vars
 int oddSerial = -1;
 int batteries = -1;
-int CARlit = -1;
+int CAR = -1;
 int FRK = -1;
 
 int main(void) {
     solveWire();
     solveButton();
+    solveMemory();
 
     return 0;
 }
--- src/memory.h
+++ src/memory.h
@@ -2,8 +2,6 @@
 #define MEMORY_H
 
 void solveMemory(void);
-int getDisplay(void);
 int getNum(void);
-int getPosition(void);
 
 #endif
